b1f83d81551eceefab8d2e273aa86c58843d58fe
Adrian Unger <dev@staydecent.ca>
2019-10-17 16:18:42 -0700

    Rough out Hapi starter project for making APIs!

---
+++ index.js
@@ -0,0 +1,63 @@
+const Hapi = require('@hapi/hapi')
+const AuthBearer = require('hapi-auth-bearer-token')
+
+const apps = [
+  require('./users')
+]
+
+const handleErr = (err) => {
+  console.log(err)
+  process.exit(1)
+}
+
+process.on('unhandledRejection', handleErr)
+
+const start = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+  })
+
+  // Setup DB
+  // ---
+
+  // @TODO: Write plugin to expose knex DB instance on hapi toolkit
+  const knex = require('knex')({
+    client: 'sqlite3',
+    connection: {
+      filename: './mydb.sqlite'
+    },
+    migrations: {
+      tableName: 'migrations'
+    }
+  })
+
+  // Setup Authentication
+  // ---
+
+  await server.register(AuthBearer)
+
+  server.auth.strategy('token', 'bearer-access-token', {
+    tokenType: 'Token',
+    validate: async (request, token, h) => {
+      // here is where you validate your token
+      // comparing with token from your database for example
+      const isValid = token === '1234'
+      const credentials = { token }
+      return { isValid, credentials }
+    }
+  })
+
+  // Default to all routes requiring Authentication.
+  // Override by providing: `options: { auth: false }` on your route.
+  server.auth.default('token')
+
+  // Load all app plugins and start the show!
+  // ---
+
+  await server.register(apps)
+  await server.start()
+  console.log('Server running on %s', server.info.uri)
+}
+
+start()
---
+++ users/index.js
@@ -0,0 +1,20 @@
+// path('users', api.Users.as_view(), name='users'),
+// path('users/<int:pk>', api.Users.as_view(), name='user'),
+// path('users/signup', api.Signup.as_view(), name='signup'),
+// path('users/login', api.Login.as_view(), name='login'),
+// path('users/forgot-password', api.ForgotPassword.as_view(), name='forgot-password'),
+// path('users/reset-password', api.ResetPassword.as_view(), name='reset-password'),
+module.exports = {
+  name: 'users',
+  version: '1.0.0',
+  register: async function (server, options) {
+    server.route({
+      method: 'GET',
+      path: '/users',
+      // options: { auth: false },
+      handler: (request, h) => {
+        return 'Hello World!'
+      }
+    })
+  }
+}
